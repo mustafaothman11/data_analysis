9b218b0e80a35bc49cd173882ce1d87457c43809
mustafaothman11 <drshahmedy029@gmail.com>
2025-10-04 10:49:32 +0300

    Delete total revenu and costs for each product.sql

--- total revenu and costs for each product.sql
+++
@@ -1,9 +0,0 @@
--- total revenu and costs for each product
-select 
- pds.product_name ,sum(price_usd) as total_revenu , sum(cogs_usd) as total_cogs
-from orders as ods 
-left join 
-products as pds
-on ods.primary_product_id = pds.product_id
-group by pds.product_name;
-
